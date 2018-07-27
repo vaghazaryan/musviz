5ed3afa480552b032f2831d3d789fec420010161
VARUZH GHAZARYAN <varuzh11@gmail.com>
2018-07-27 11:56:02 +0400

    connect to threejs

---
+++ src/js/dots.js
@@ -0,0 +1,164 @@
+
+var SCREEN_WIDTH = window.innerWidth,
+    SCREEN_HEIGHT = window.innerHeight,
+
+    mouseX = 0, mouseY = 0,
+
+    windowHalfX = window.innerWidth / 2,
+    windowHalfY = window.innerHeight / 2,
+
+    SEPARATION = 200,
+    AMOUNTX = 10,
+    AMOUNTY = 10,
+    zoom = 1,
+
+    camera, scene, renderer;
+
+init();
+animate();
+
+function init() {
+
+    var container, separation = 100, amountX = 50, amountY = 50,
+        particles, particle;
+
+    container = document.createElement('div');
+    container.classList.add('aura');
+    document.body.appendChild(container);
+
+    camera = new THREE.PerspectiveCamera( 75, SCREEN_WIDTH / SCREEN_HEIGHT, 1, 10000 );
+    camera.position.z = 1000;
+
+    scene = new THREE.Scene();
+
+    renderer = new THREE.CanvasRenderer();
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
+    container.appendChild( renderer.domElement );
+
+    // particles
+
+    var PI2 = Math.PI * 2;
+    var material = new THREE.SpriteCanvasMaterial( {
+
+        color: 0xffffff,
+        program: function ( context ) {
+
+            context.beginPath();
+            context.arc( 0, 0, 0.5, 0, PI2, true );
+            context.fill();
+
+        }
+
+    } );
+
+    for ( var i = 0; i < 1000; i ++ ) {
+
+        particle = new THREE.Sprite( material );
+        particle.position.x = Math.random() * 2 - 1;
+        particle.position.y = Math.random() * 2 - 1;
+        particle.position.z = Math.random() * 2 - 1;
+        particle.position.normalize();
+        particle.position.multiplyScalar( Math.random() * 10 + 450 );
+        particle.scale.multiplyScalar( 2 );
+        scene.add( particle );
+
+    }
+
+    // lines
+
+    for (var i = 0; i < 300; i++) {
+
+        var geometry = new THREE.Geometry();
+
+        var vertex = new THREE.Vector3( Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1 );
+        vertex.normalize();
+        vertex.multiplyScalar( 450 );
+
+        geometry.vertices.push( vertex );
+
+        var vertex2 = vertex.clone();
+        vertex2.multiplyScalar( Math.random() * 0.3 + 1 );
+
+        geometry.vertices.push( vertex2 );
+
+        var line = new THREE.Line( geometry, new THREE.LineBasicMaterial( { color: 0xffffff, opacity: Math.random() } ) );
+        scene.add( line );
+    }
+
+    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+    document.addEventListener( 'touchstart', onDocumentTouchStart, false );
+    document.addEventListener( 'touchmove', onDocumentTouchMove, false );
+
+    //
+
+    window.addEventListener( 'resize', onWindowResize, false );
+
+}
+
+function onWindowResize() {
+
+    windowHalfX = window.innerWidth / 2;
+    windowHalfY = window.innerHeight / 2;
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+//
+
+function onDocumentMouseMove(event) {
+
+    mouseX = event.clientX - windowHalfX;
+    mouseY = event.clientY - windowHalfY;
+}
+
+function onDocumentTouchStart( event ) {
+
+    if ( event.touches.length > 1 ) {
+
+        event.preventDefault();
+
+        mouseX = event.touches[ 0 ].pageX - windowHalfX;
+        mouseY = event.touches[ 0 ].pageY - windowHalfY;
+
+    }
+
+}
+
+function onDocumentTouchMove( event ) {
+
+    if ( event.touches.length == 1 ) {
+
+        event.preventDefault();
+
+        mouseX = event.touches[ 0 ].pageX - windowHalfX;
+        mouseY = event.touches[ 0 ].pageY - windowHalfY;
+
+    }
+
+}
+
+//
+
+function animate() {
+
+    requestAnimationFrame( animate );
+    camera.fov = zoom;
+    render();
+
+}
+var x = 800,
+    y = 800,
+    z = 800,
+    c = 1
+function render() {
+    camera.position.set(x - (totalPoints * 1.5), y  - (totalPoints * 1.5), z - (totalPoints * 1.5));
+    scene.rotation.x += totalPoints / 30000;
+    camera.lookAt( scene.position);
+
+    renderer.render( scene, camera );
+}
--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,13 @@ var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 var audioElement = document.getElementById('audioElement');
 var audioSrc = audioCtx.createMediaElementSource(audioElement);
 var analyser = audioCtx.createAnalyser();
+var bassPoint, totalPoints;
 
 audioSrc.connect(analyser);
 audioSrc.connect(audioCtx.destination);
 
 var frequencyData = new Uint8Array(200);
+var frequencyDataDrums = new Uint8Array(1);
 
 function renderAnimation() {
     requestAnimationFrame(renderAnimation);
@@ -16,8 +18,10 @@ function renderAnimation() {
 }
 
 function bassAnimation(data) {
+    console.log(frequencyData)
     let maxPoint = 255;
-    let bassPoint = data[3];
+    bassPoint = data[2];
+    totalPoints = data.reduce(function(acc, val) { return acc + val; }, 0) / data.length
     let scale = bassPoint / 100;
     let img = document.querySelector('.music_media');
     img.style.transform =  `scale(${scale >=1 ? scale : 1})`;
